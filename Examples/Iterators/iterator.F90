e2a8e8a5d07d6fe12dda8609b30e8d303e7e7526
Tom Clune <thomas.l.clune@nasa.gov>
2021-08-25 16:14:25 -0400

    A bit of cleanup.
    Als showed the correct way to handle errors in the iterator example.

--- Examples/Iterators/iterator.F90
+++ Examples/Iterators/iterator.F90
@@ -10,95 +10,91 @@ program main
 
    type(Parser) :: p
    type(Configuration) :: config
-   type(Configuration) :: subcfg, subsubcfg
-   type(Configuration) :: shape_cfg
-
-   type(MappingIterator) :: iter, iter2
-   type(Sequence), pointer :: s
-
    integer :: status
-   class(AbstractNode), pointer :: node
-   integer :: prime
-   character(:), allocatable :: shape, key
-   integer :: n_edges
-   integer :: i
-   type(Mapping), pointer :: m
 
    p = Parser('core')
-   config = p%load(FileStream('animal.yaml'))
-   write(10,'(dt)', iostat=status) config
-
    config = p%load(FileStream('iterator.yaml'))
    write(10,'(dt)', iostat=status) config
 
-   ! Iterating over a sequence
-   subcfg = config%of('primes')
-   do i = 1, subcfg%size()
-      prime = subcfg%of(i)
-      print*,'prime: ', prime
-   end do
-   print*,subcfg
+   call optimistic(config)
+   call pessimistic(config)
 
-   ! Iterating over a mapping
-   subcfg = config%of('shapes')
-   associate (b => subcfg%begin(), e => subcfg%end())
-     iter = b
-     do while (iter /= e)
+contains
+   
+   ! This procedure does not check any potential errors
+   ! but is easier to follow as a result.
+   subroutine optimistic(config)
+      type(Configuration), intent(in) :: config
 
-        node => iter%first()
-        shape = node
-!!$        shape = iter%first() ! should return a subconfig
-        node => iter%second()
-        call node%get(n_edges, 'num_edges')
-!!$        n_edges = node%of('num_edges')
-        print*,'Shape: ', shape, ' has ', n_edges, 'sides.'
-        call iter%next()
-        
-     end do
-   end associate
+      integer :: i
+      integer :: prime
+      type(Configuration) :: subcfg
+      type(MappingIterator) :: iter
+      class(AbstractNode), pointer :: node
+      character(:), allocatable :: shape, key
+      integer :: n_edges
+
+      ! Iterating over a sequence
+      subcfg = config%of('primes')
+      do i = 1, subcfg%size()
+         prime = subcfg%of(i)
+         print*,'prime: ', prime
+      end do
+      
+      ! Iterating over a mapping
+      subcfg = config%of('shapes')
+      associate (b => subcfg%begin(), e => subcfg%end())
+        iter = b
+        do while (iter /= e)
+           shape = iter%first() ! key
+           node => iter%second() ! value
+           call node%get(n_edges, 'num_edges')
+           print*,'Shape: ', shape, ' has ', n_edges, 'sides.'
+           call iter%next()
+        end do
+      end associate
+   end subroutine optimistic
 
    
-   ! Outer loop over mapping
-   m => to_mapping(config%node)
-   associate(b => m%begin(), e=> m%end())
-     iter = b
-     do while (iter /= e)
+   ! This procedure carefully checks all return codes.
+   subroutine pessimistic(config)
+      type(Configuration), intent(in) :: config
 
-        ! Access mapping values with key()/value() methods on iterator.
-!!$        key = iter%first()
-!!$        
-!!$        select case (key)
-!!$        case ('primes')
-!!$           
-!!$           ! We expect a yaml sequence here
-!!$           node => iter%second()
-!!$           
-!!$           do i = 1, node%size()
-!!$              prime = node%of(i)
-!!$              print*,'another prime: ', prime
-!!$           end do
-!!$           
-!!$        case ('shapes')
-!!$           
-!!$           ! We expect a yaml mapping here  (shape: #sides)
-!!$           node => iter%second()
-!!$           m => to_mapping(node)
-!!$
-!!$           associate (bm => m%begin(), be => m%end())
-!!$             iter2 = bm
-!!$             do while (iter2 /= be)
-!!$                shape = iter2%first()
-!!$                n_edges = node%of(shape,'num_edges')
-!!$                print*,'shape: ',shape,' has ',n_edges, 'sides'
-!!$                call iter2%next()
-!!$             end do
-!!$           end associate
-!!$           
-!!$        end select
-        call iter%next()
-     end do
-   end associate
+      integer :: i
+      integer :: prime
+      type(Configuration) :: subcfg
+      type(MappingIterator) :: iter
+      class(AbstractNode), pointer :: node
+      character(:), allocatable :: shape, key
+      integer :: n_edges
+      integer :: status
+
+      ! Iterating over a sequence
+      subcfg = config%at('primes', rc=status)
+      if (status /= YAFYAML_SUCCESS) return
 
       
+      do i = 1, subcfg%size()
+         prime = subcfg%at(i, rc=status)
+         if (status /= YAFYAML_SUCCESS) return
+         print*,'prime: ', prime
+      end do
+      
+      ! Iterating over a mapping
+      subcfg = config%at('shapes', rc=status)
+      if (status /= YAFYAML_SUCCESS) return
+      associate (b => subcfg%begin(), e => subcfg%end())
+        iter = b
+        do while (iter /= e)
+           shape = iter%first() ! key
+           node => iter%second() ! value
+           call node%get(n_edges, 'num_edges', rc=status)
+           if (status /= YAFYAML_SUCCESS) return
+           print*,'Shape: ', shape, ' has ', n_edges, 'sides.'
+           call iter%next()
+        end do
+      end associate
+
+   end subroutine pessimistic
 
 end program main

b15e3f770c727763241a8557802411ba7d3bd219
Tom Clune <thomas.l.clune@nasa.gov>
2023-01-26 13:05:59 -0500

    Update src/Nodes/Nodes.F90

--- src/Nodes/Nodes.F90
+++ src/Nodes/Nodes.F90
@@ -27,8 +27,6 @@ contains
       logical, intent(out) :: flag
       class(YAML_Node), intent(in) :: this
 
-      logical, pointer :: ptr
-
       flag = to_bool(this)
 
    end subroutine assign_to_logical
